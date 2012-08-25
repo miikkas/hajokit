5d0b3ae0327c0c28c6c97693ad4befeb03a71dff
Miikka Salminen <miikka.salminen@gmail.com>
2012-08-25 15:40:28 +0300

    Set up the template directory and added some initial urls.

--- django-project/draw_and_guess/urls.py
+++ django-project/draw_and_guess/urls.py
@@ -1,17 +1,16 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'draw_and_guess.views.home', name='home'),
-    # url(r'^draw_and_guess/', include('draw_and_guess.foo.urls')),
+    url(r'^$', 'game.views.index'),
+    url(r'^players/$', 'game.views.players'),
+    url(r'^canvas/$', 'game.views.canvas'),
+    url(r'^guesses/$', 'game.views.guesses'),
+    url(r'^guess/$', 'game.views.guess'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    # The admin URL:
+    url(r'^admin/', include(admin.site.urls)),
 )
