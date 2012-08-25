62bf01dad3cc3631710794f272ae3a9bfaf0f0ca
Miikka Salminen <miikka.salminen@gmail.com>
2012-08-25 15:44:32 +0300

    Funktiorungot viewejä varten.

--- django-project/game/views.py
+++ django-project/game/views.py
@@ -1,1 +1,22 @@
-# Create your views here.
+import base64
+
+def index():
+    """For HTTP GETting the index page of the application."""
+    pass
+
+def players():
+    """For HTTP GETting the data of the current players."""
+    pass
+
+def canvas():
+    """For HTTP GETting the current canvas data, base 64 encoded."""
+    pass
+
+def guesses():
+    """For HTTP GETting the guesses made on the current game."""
+    pass
+
+def guess():
+    """For HTTP POSTing a guess to the current game."""
+    pass
+
