fd0e027e5df98372383d5524d1b09da2b228cff6
Miikka Salminen <miikka.salminen@gmail.com>
2012-08-25 16:03:41 +0300

    Lisätty json-kirjaston importtaus vieweihin.

--- django-project/game/views.py
+++ django-project/game/views.py
@@ -1,22 +1,26 @@
 import base64
+import json
+
+from django.http import HttpResponse
+from django.template import RequestContext
 
 def index(request):
     """For HTTP GETting the index page of the application."""
     pass
 
 def players(request):
-    """For HTTP GETting the data of the current players."""
+    """For HTTP GETting the data of the current players, JSON encoded."""
     pass
 
 def canvas(request):
-    """For HTTP GETting the current canvas data, base 64 encoded."""
+    """For HTTP GETting the current canvas data, base 64 encoded, JSON encoded."""
     pass
 
 def guesses(request):
-    """For HTTP GETting the guesses made on the current game."""
+    """For HTTP GETting the guesses made on the current game, JSON encoded."""
     pass
 
 def guess(request):
-    """For HTTP POSTing a guess to the current game."""
+    """For HTTP POSTing a guess to the current game, JSON encoded(?)."""
     pass
 
