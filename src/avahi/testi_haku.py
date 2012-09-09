f5681d9c478586e6f394a9b9f3dec79ce6a0ae1b
Ilkka Ollakka <ileoo@ee.oulu.fi>
2012-09-09 12:52:44 +0300

    avahi: eka koodi joka huomiois palvelun poistumisen

--- src/avahi/testi_haku.py
+++ src/avahi/testi_haku.py
@@ -7,22 +7,43 @@ from dbus.mainloop.glib import DBusGMainLoop
 
 TYPE = '_hajarit._tcp'
 
+servicelist = {}
+
 def service_resolved(*args):
     print 'service resolved'
     print 'name:',args[2]
     print 'address:',args[7]
     print 'port:',args[8]
     print 'txt:',avahi.txt_array_to_string_array(args[9])
+    servicelist{args[2]}=[args[7],avahi.txt_array_to_string_array(args[9])]
+
+def service_removed(*args):
+    servicelist{args[2]}=None
+
+    print "Remaining services:"
+    print servicelist
 
 def print_error(*args):
     print 'error handler'
     print args[0]
 
-def myhandler( interface, protocol, name, stype, domain, flags):
+def remove_service( interface, protocol, name, stype, domain, flags):
+    print 'Removing service "%s" type "%s" domain "%s" ' %(name, stype, domain)
+
+    if flags & avahi.LOOKUP_RESULT_LOCAL:
+        pass
+
+    server.ResolveService(interface, protocol, name, stype,
+        domain, avahi.PROTO_UNSPEC, dbus.UInt32(0),
+        reply_handler=service_removed, error_handler=print_error)
+
+
+
+def new_service( interface, protocol, name, stype, domain, flags):
     print 'Found service "%s" type "%s" domain "%s" ' %(name,stype,domain)
 
-    #if flags & avahi.LOOKUP_RESULT_LOCAL:
-    #    pass
+    if flags & avahi.LOOKUP_RESULT_LOCAL:
+        pass
 
     server.ResolveService(interface, protocol, name, stype,
         domain, avahi.PROTO_UNSPEC, dbus.UInt32(0),
@@ -40,6 +61,7 @@ sbrowser = dbus.Interface( bus.get_object(avahi.DBUS_NAME,
                     avahi.PROTO_UNSPEC, TYPE, 'local', dbus.UInt32(0))),
                avahi.DBUS_INTERFACE_SERVICE_BROWSER)
 
-sbrowser.connect_to_signal("ItemNew", myhandler)
+sbrowser.connect_to_signal("ItemNew", new_service)
+sbrowser.connect_to_signal("ItemRemove", remove_service)
 
 gobject.MainLoop().run()
