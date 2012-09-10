c3a9b8dfcdfe137af7499b60a41fb9de27afc1ed
Ilkka Ollakka <ileoo@ee.oulu.fi>
2012-09-11 00:16:02 +0300

    SD: Päivitä djangon sqlite kantaan löydetyt nodet

--- src/avahi/testi_haku.py
+++ src/avahi/testi_haku.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 # -*- encoding:utf-8 -*-
 
-import dbus, gobject, avahi
+import dbus, gobject, avahi, sqlite3
 from dbus import DBusException
 from dbus.mainloop.glib import DBusGMainLoop
 
 TYPE = '_hajarit._tcp'
+
+#django sqlite kanta
+conn = sqlite3.connect('/home/hajokit/hajokit/django-project/dag.db')
+
+cursor = conn.cursor()
 
 servicelist = {}
 
@@ -13,11 +18,17 @@ def print_services():
     print "Services:"
     for service in servicelist.keys():
         print "https://%s.local:%s%s" % (service,servicelist[service][0],servicelist[service][1]["path"])
+    print "SQL-kannasta:"
+    for service in cursor.execute('SELECT * FROM game_pelinode '):
+        print service
 
 def service_resolved(*args):
     if args[2] not in servicelist:
         txt = dict(item.split('=') for item in avahi.txt_array_to_string_array(args[9]))
         servicelist[args[2]]=[args[8],txt]
+        serviceentry = (args[2]+".local",args[8],txt["path"])
+        cursor.execute('INSERT INTO game_pelinode(hostname,port,path) VALUES(?,?,?)', serviceentry)
+        conn.commit()
         print_services()
 
 
@@ -30,6 +41,8 @@ def remove_service( interface, protocol, name, stype, domain, flags):
     if flags & avahi.LOOKUP_RESULT_LOCAL:
         pass
 
+    cursor.execute('DELETE FROM game_pelinode where hostname = ?',(name+".local",))
+    conn.commit()
     if name in servicelist:
        print 'Removing service "%s" type "%s" domain "%s" ' %(name, stype, domain)
        del servicelist[name]
