610b02a536398a757be08ca0700bc656ade5f45b
Ilkka Ollakka <ileoo@ee.oulu.fi>
2012-09-10 12:41:35 +0300

    Eka yritelmä django malleihin

--- django-project/game/models.py
+++ django-project/game/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+#Pelinode, eli virtuaali kone jossa joku pelaaja on kiinni
+class PeliNode(models.Model):
+    hostname = models.CharField(max_length=256)
+    port     = models.PositiveIntegerField()
+    path     = models.CharField(max_length=256)
+
+#Pelaaja, pidetään tallessa missä nodessa on kiinni ja tunniste
+class Pelaaja(models.Model):
+    pelinode = models.ForeingKey('PeliNode')
+    nimi     = models.CharField(max_length=256)
+
+#Muutos on Piirrokseen tulleet muutokset
+class Muutos(models.Model):
+    muutos   = models.TextField()
+    alku     = models.ForeingKey('Piirros')
+
+#Piirros on base64 koodattu piirrostilanne, tämän lisäksi muutokset
+# kun pelaaja liittyy
+class Piirros(models.Model):
+    tilanne  = models.TextField()
+
+#Pelitilanne, kuka on piirtomuodossa ja mitä on piirretty
+class Peli(models.Model):
+    piirtaja = models.ForeingKey('Pelaaja')
+    tilanne  = models.ForeingKey('Piirros')
