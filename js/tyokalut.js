d34ef77c69216de4cffd93888b03a23592d53112
Niko Tomperi <niko.tomperi@gmail.com>
2012-09-11 19:42:52 +0300

    paper.js ja siihen pohjautuvat uudet työkalut

---
+++ js/tyokalut.js
@@ -0,0 +1,51 @@
+﻿paper.install(window);
+/*
+ * Globaalit muuttujat, jotta niitä voi muokata html:stä käsin.
+ */
+
+var kyna, viiva, nelikulmio, ellipsi, kumi, taytto;
+var vari = 'black';
+
+window.onload = function() {
+    paper.setup('canvas');
+    var path;
+
+    //Igor, kynä!
+    kyna = new Tool();
+    kyna.onMouseDown = function(event) {
+        path = new Path();
+        path.strokeColor = vari;
+    }
+    kyna.onMouseDrag = function(event) {
+        path.add(event.point);
+    }
+    kyna.onMouseUp = function(event) {
+        path.simplify();
+    }
+    
+    //Viiva
+    viiva = new Tool();
+    viiva.onMouseDown = function(event) {
+        path = new Path();
+        path.strokeColor = vari;
+        if (path.segments.length == 0) {
+            path.add(event.point);
+        }
+        path.add(event.point);
+    }
+    viiva.onMouseDrag = function(event) {
+        path.lastSegment.point = event.point;
+    }
+    viiva.onMouseUp = function(event) {
+        path.add(event.point);
+    }
+
+
+}
+
+function changeColor(color) {
+    /*
+     * Vaihda kalun väriä colorin mukaiseksi.
+     */
+    vari = color;
+}
