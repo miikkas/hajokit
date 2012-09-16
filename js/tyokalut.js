b6d1399ee7cce477d4e8302b3c31589664fedf29
Niko Tomperi <niko.tomperi@gmail.com>
2012-09-16 17:25:37 +0300

    Koonmuutostoiminnallisuus, yhdenmukaistusta
    Funktio työkalun koon muuttamiseksi, turhat englannit suomeksi

--- js/tyokalut.js
+++ js/tyokalut.js
@@ -4,71 +4,79 @@
  */
 
 var kyna, viiva, nelikulmio, ellipsi, kumi, taytto;
-var vari = 'black';
+var piirtovari = 'black';
+var piirtokoko = 5;
 
 window.onload = function() {
-    paper.setup('piirtocanvas');
-    var path;
+    paper.setup('canvas');
+    kyna = new Tool();
+    viiva = new Tool();
+    ympyra = new Tool();
+    nelikulmio = new Tool();
+    var polku;
+    var alkuPiste;
 
     //Igor, kynä!
-    kyna = new Tool();
     kyna.onMouseDown = function(event) {
-        path = new Path();
-        path.strokeColor = vari;
+        polku = new polku();
+        polku.strokeColor = piirtovari;
+        polku.strokeWidth = piirtokoko;
     }
     kyna.onMouseDrag = function(event) {
-        path.add(event.point);
+        polku.add(event.point);
     }
     kyna.onMouseUp = function(event) {
-        path.simplify();
+        polku.simplify();
     }
     
     //Viiva
-    viiva = new Tool();
     viiva.onMouseDown = function(event) {
-        path = new Path();
-        path.strokeColor = vari;
-        if (path.segments.length == 0) {
-            path.add(event.point);
+        polku = new polku();
+        polku.strokeColor = piirtovari;
+        polku.strokeWidth = piirtokoko;
+        if (polku.segments.length == 0) {
+            polku.add(event.point);
         }
-        path.add(event.point);
+        polku.add(event.point);
     }
     viiva.onMouseDrag = function(event) {
-        path.lastSegment.point = event.point;
+        polku.lastSegment.point = event.point;
     }
     viiva.onMouseUp = function(event) {
-        path.add(event.point);
+        polku.add(event.point);
     }
 
     //Ympyrä
-    ympyra = new Tool();
-    var startPoint;
     ympyra.onMouseDown = function(event) {
-        startPoint = event.point;
+        alkuPiste = event.point;
     }
     ympyra.onMouseDrag = function(event) {
         var rad = startPoint.getDistance(event.point, false);
-        path = new Path.Circle(startPoint, rad);
-        path.strokeColor = vari;
-        path.removeOnDrag();
+        polku = new polku.Circle(alkuPiste, rad);
+        polku.strokeColor = piirtovari;
+        polku.strokeWidth = piirtokoko;
+        polku.removeOnDrag();
     }
 
     //Nelikulmio
-    nelikulmio = new Tool();
-    var startPoint;
     nelikulmio.onMouseDown = function(event) {
-        startPoint = event.point;
+        alkuPiste = event.point;
     }
     nelikulmio.onMouseDrag = function(event) {
-        path = new Path.Rectangle(startPoint, event.point);
-        path.strokeColor = vari;
-        path.removeOnDrag();
+        polku = new polku.Rectangle(alkuPiste, event.point);
+        polku.strokeColor = piirtovari;
+        polku.strokeWidth = piirtokoko;
+        polku.removeOnDrag();
     }
 }
 
-function changeColor(color) {
+function muutaVaria(vari) {
     /*
      * Vaihda kalun väriä colorin mukaiseksi.
      */
-    vari = color;
+    piirtovari = vari;
+}
+
+function muutaKokoa(koko) {
+    piirtokoko = koko;
 }
