1b192cc404ce9aba6178e10ee1850c81c755cce5
Niko Tomperi <niko.tomperi@gmail.com>
2012-09-23 20:10:13 +0300

    säätöä

--- js/comm.js
+++ js/comm.js
@@ -1,64 +1,47 @@
-﻿paper.install(window);
-var drawView;
-
-window.onload = function() {
-    drawView = new View('drawcanvas');
-    paper.setup('drawingcanvas');
-};
-
-function sendCanvas() {
-    /*
-     * Send canvas contents to server.
-     */
-    
-    var canvas = document.getElementById('drawingcanvas');
-    var image = canvas.toDataURL("image/png");
-    //Not expecting anything meaningful back from the server.
-    $.ajax ({
-        type: "POST",
-        url: "canvas/jokuid",
-        dataType: "text", 
-        data: 'image=' + image
-    }).done(function (response) {
-        //
+﻿$(document).ready(function () {
+    var player = 'gunther';
+    $("#guessbox").keyup(function(event){
+        if(event.keyCode == 13){
+            sendGuess(player, $('#guessbox').val());
+        }
     });
-}
-function getDiff(path) {
-    /*
-     * GET the latest path that was drawn from the server in JSON.
-     */
-    
-    //Do something with the id.
-    $.ajax ({
-        type: "GET",
-        url: "canvas/jokuid/",
-        dataType: "json", 
-    }).done(function (response) {
-        drawDiff(response);
+    var canvaso = document.getElementById('drawingcanvas');
+    nakyma = new View('drawingcanvas');
+    var image;
+    contexto = canvaso.getContext('2d');
+    $("#button").live("click", function(event){
+        sendCanvas();
+        //alert('vitun vittu');
     });
-}
-function drawDiff(diff) {
-    /*
-     * Draw a path of segments in the given diff, 
-     * with the given color and size.
-     */
-    
-    var path = new Path();
-    var point, handleIn, handleOut;
-    path.strokeColor = diff.color;
-    path.strokeWidth = diff.size;
-    $.each(diff.segments, function(key,valueObj){
-        point = new Point(valueObj.pointx, valueObj.pointy);
-        handleIn = new Point(valueObj.handleInx, valueObj.handleIny);
-        handleOut = new Point(valueObj.handleOutx, valueObj.handleOuty);
-        path.add(point, handleIn, handleOut);
-    });
-    view.draw();
-}
-
-function reDraw() {
-    /*
-     * Draw the canvas so that any new paths actually show up.
-     */
-    view.draw();
-}
+    function sendGuess(playername, guessword) {
+        
+        var guess = {playername: guessword};
+        $.ajax ({
+            type: "POST",
+            url: "guess",
+            dataType: json, 
+            data: guess
+        }).done(function (response) {
+            //Add guess to a list showing all guesses so far
+            /*if (guess was correct) {
+                say hurray, end game
+            }*/
+        });
+    }
+    function sendCanvas() {
+        /*
+         * Send canvas contents to server.
+         */
+        var canvas = document.getElementById('drawingcanvas');
+        var image = canvas.toDataURL("image/png");
+        var data = [{"pk": 1, "model": "game.piirros", "fields": {"aikaleima": "2012-09-23T16:45:19.388Z", "tilanne": image}}]
+        //Not expecting anything meaningful back from the server.
+        $.ajax ({
+            type: "POST",
+            url: "canvas/1",
+            data: image
+        }).done(function (response) {
+            alert('yay: ' + response);
+        });
+    }
+});
