08c21ce24ba88f637bd3a49c2988f072977eaaa5
Niko Tomperi <niko.tomperi@gmail.com>
2012-09-25 16:43:33 +0300

    asdfgfa

--- js/diffpoll.js
+++ js/diffpoll.js
@@ -6,6 +6,54 @@
  */
 
 $(document).ready(function () {
+    function getDiff() {
+        /*
+         * Get a new path to be drawn from the server using long 
+         * polling.
+         */
+        
+        $.ajax ({
+            type: "GET",
+            url: "canvas/1/diff",
+            dataType: "json"
+        }).done(function (response, textStatus, xhr) {
+            // Server responds with 304 status code, if there's 
+            // nothing new to draw.
+            if (xhr.status != 304) {
+                window.console.log('data saatiin');
+                drawDiff(response);
+            }
+            //getDiff();
+        });
+    }
+
+    function drawDiff(json) {
+        /*
+         * Draw a path described in the diff on the canvas.
+         */
+        
+        var path = new Path();
+        var point, handleIn, handleOut;
+        window.console.log('nyt sopis parsia');
+        /*$.each(json, function(key,valueObj){
+            console.log(json[i].model);
+            /*path.strokeColor = json.color;
+            path.strokeWidth = json.size;
+            point = new Point(valueObj.pointx, valueObj.pointy);
+            handleIn = new Point(valueObj.handleInx, valueObj.handleIny);
+            handleOut = new Point(valueObj.handleOutx, valueObj.handleOuty);
+            path.add(point, handleIn, handleOut);*/
+        //});
+        //view.draw();
+    }
+
+    function reDraw() {
+        /*
+         * Redraw the canvas to show any changes.
+         */
+        view.draw();
+    }    
+
     paper.install(window);
     drawView = new View('piirtocanvas');
     window.console.log('testataan konsolin toimintaa saatana');
@@ -19,50 +67,3 @@ $(document).ready(function () {
     });*/
 });
 
-function getDiff() {
-    /*
-     * Get a new path to be drawn from the server using long 
-     * polling.
-     */
-    
-    $.ajax ({
-        type: "GET",
-        url: "canvas/1/diff",
-        dataType: "json"
-    }).done(function (response, textStatus, xhr) {
-        // Server responds with 304 status code, if there's 
-        // nothing new to draw.
-        if (xhr.status != 304) {
-            window.console.log('data saatiin');
-            drawDiff(response);
-        }
-        //getDiff();
-    });
-}
-
-function drawDiff(json) {
-    /*
-     * Draw a path described in the diff on the canvas.
-     */
-    
-    var path = new Path();
-    var point, handleIn, handleOut;
-    window.console.log('nyt sopis parsia');
-    /*$.each(json, function(key,valueObj){
-        console.log(json[i].model);
-        /*path.strokeColor = json.color;
-        path.strokeWidth = json.size;
-        point = new Point(valueObj.pointx, valueObj.pointy);
-        handleIn = new Point(valueObj.handleInx, valueObj.handleIny);
-        handleOut = new Point(valueObj.handleOutx, valueObj.handleOuty);
-        path.add(point, handleIn, handleOut);*/
-    //});
-    //view.draw();
-}
-
-function reDraw() {
-    /*
-     * Redraw the canvas to show any changes.
-     */
-    view.draw();
-}
