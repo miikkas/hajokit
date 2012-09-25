12febe09b72455bad23bbf4ba870a79afcf356b6
Niko Tomperi <niko.tomperi@gmail.com>
2012-09-25 15:45:21 +0300

    png-sählsähl pois

--- js/comm.js
+++ js/comm.js
@@ -9,10 +9,7 @@
     nakyma = new View('drawingcanvas');
     var image;
     contexto = canvaso.getContext('2d');
-    $("#button").live("click", function(event){
-        sendCanvas();
-        //alert('vitun vittu');
-    });
+
     function sendGuess(playername, guessword) {
         
         var guess = {playername: guessword};
@@ -26,30 +23,6 @@
             /*if (guess was correct) {
                 say hurray, end game
             }*/
-        });
-    }
-    function sendCanvas() {
-        /*
-         * Send canvas contents to server.
-         */
-        var canvas = document.getElementById('drawingcanvas');
-        var image = canvas.toDataURL("image/png");
-        //alert(image);
-        var data = [{"pk": "1", "model": "game.piirros", "fields": {"aikaleima": "2012-09-23T16:45:19.388Z", "tilanne": image}}];
-        //alert(datashit[1].aikaleima);
-        //Not expecting anything meaningful back from the server.
-        $.ajax ({
-            type: "POST",
-            url: "canvas/1/",
-            dataType: "json", 
-            data: data
-        }).done(function (response) {
-            alert('yay: ' + response);
-        }).fail(function(response) {
-            alert('ei mennyt nappiin'); 
-            $.each(response, function(key,valueObj){
-                console.log(key + ': ' + valueObj);
-            });
         });
     }
 });
