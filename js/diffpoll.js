73534ecb00978cfc04e5654496d8cff15757285e
Niko Tomperi <niko.tomperi@gmail.com>
2012-09-28 18:20:43 +0300

    errorio pukkoo, jäljitellöö

--- js/diffpoll.js
+++ js/diffpoll.js
@@ -27,9 +27,9 @@ function getGameID() {
         //window.console.log(response);
         try {
             id = jQuery.parseJSON(response)[0].fields.canvas;
-            jQuery.data(document.body, 'canvasid', jQuery.parseJSON(json)[0].fields.canvas);
+            jQuery.data(document.body, 'canvasid', id);
         } catch (e) {
-            window.console.log('Lord Inglip, I have failed to complete my task to acquire an ID for the game.');
+            window.console.log('Lord Inglip, I have failed to complete my task to acquire an ID for the game: ' + e);
         }
         //window.console.log('Got ID ' + id);
         getDiff(id, 0);
