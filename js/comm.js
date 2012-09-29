5a5bb0d59d5db6c9c6c554793304bad82b628e11
Niko Tomperi <niko.tomperi@gmail.com>
2012-09-29 09:44:41 +0300

    arvauspollaus

--- js/comm.js
+++ js/comm.js
@@ -1,17 +1,4 @@
-﻿$(document).ready(function () {
-    var player = 'gunther';
-    $("#arvaussyotto").keyup(function(event){
-        if(event.keyCode == 13){
-            sendGuess(player, $('#arvaussyotto').val());
-	    $('#arvaussyotto').val("");
-        }
-    });
-    var canvaso = document.getElementById('drawingcanvas');
-    nakyma = new View('drawingcanvas');
-    var image;
-    contexto = canvaso.getContext('2d');
-
-    function sendGuess(playername, guessword) {
+﻿    function sendGuess(playername, guessword) {
         
         var guess = {playername: guessword};
         $.ajax ({
@@ -19,30 +6,47 @@
             url: "guess",
             dataType: json, 
             data: guess
-        }).done(function (response) {
-            //Add guess to a list showing all guesses so far
-            /*if (guess was correct) {
-                say hurray, end game
-            }*/
+        }).fail(function (response, textStatus, xhr) {
+            console.log('Failed to send message: ' + response + ', ' + textStatus);
+            //TO-DO: resend?
         });
     }
 
-    
-// tähän kohti tulee palvelimelta kysely v
-    function getGuesses(playername, guessword) {
-        
-        var guess = {playername: guessword};
-        $.ajax ({
-            type: "GET",
-            url: "guess",
-            dataType: json, 
-            data: guess
-        }).done(function (response) {
-            //Add guess to a list showing all guesses so far
-            /*if (guess was correct) {
-                say hurray, end game
-            }*/
-        });
-    }  
-  
+function getGuesses(timestamp) {
+    var next_timestamp = timestamp;
+    var url = "guesses/" + timestamp;
+    $.ajax ({
+        type: "GET",
+        url: url,
+        dataType: "text",
+        complete: function(){getGuesses(next_timestamp);},
+        timeout: 60000
+    }).done(function (response, textStatus, xhr) {
+        // Server responds with 304 status code, if there's 
+        // nothing new to draw.
+        if (xhr.status == 200) {
+            try {
+                window.console.log(response);
+                //var jason = jQuery.parseJSON(response);
+                //next_timestamp = appendGuesses(jason);
+            }
+            catch (e) {
+                window.console.log('Error while getting the latest messages: ' + e);
+            }
+        }
+        else {
+            window.console.log(xhr.status + ' occurred while getting the latest messages.');
+        }
+    });
+}
+
+$(document).ready(function () {
+    getGuesses(0);
+    var player = 'gunther';
+    $("#arvaussyotto").keyup(function(event){
+        if(event.keyCode == 13){
+            sendGuess(player, $('#arvaussyotto').val());
+	    $('#arvaussyotto').val("");
+        }
+    });
 });
